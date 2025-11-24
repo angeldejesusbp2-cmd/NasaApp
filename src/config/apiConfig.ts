156387ec9c89ecd4951e1d94fa021e148471b607
Angel Sanchez <angeldejesusbp2@gmail.com>
2025-11-24 07:56:19 -0600

    Guardar cambios en apiConfig

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,5 +1,4 @@
-// Reemplaza esto con tu clave API de NASA
-// Obtén tu clave en: https://api.nasa.gov/
+
 export const NASA_API_KEY = 'AN0XyshYyLrSaQVPlnAqgrTRs1nif9MtVdxIGyFG';
 
 // Configuración de endpoints
